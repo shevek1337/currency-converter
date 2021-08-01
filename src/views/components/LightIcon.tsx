48ea839be51f50809bf7da436fc347ec2f6ef0f1
Gigi Tophuria <gtophuria@gmail.com>
2021-08-01 17:01:47 +0400

    minor code style changes

--- src/views/components/LightIcon.tsx
+++ src/views/components/LightIcon.tsx
@@ -1,6 +1,6 @@
-import React from "react";
+import React, { SVGProps } from "react";
 
-const LightIcon = ({ ...props }: React.SVGProps<SVGSVGElement>) => {
+const LightIcon = ({ ...props }: SVGProps<SVGSVGElement>) => {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
