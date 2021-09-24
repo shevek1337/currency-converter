6305409800475157f647eed4dd7ed89ea80eb8b9
Gigi Tophuria <gtophuria@gmail.com>
2021-09-24 20:57:41 +0400

    exported all flags from index

--- src/imgs/countries/index.ts
+++ src/imgs/countries/index.ts
@@ -1,4 +1,91 @@
 import Georgia from "./georgia.svg"
 import Usa from "./usa.svg"
+import Armenia from "./armenia.svg"
+import Australia from "./australia.svg"
+import Azerbaijan from "./azerbaijan.svg"
+import Belarus from "./belarus.svg"
+import Brazil from "./brazil.svg"
+import Bulgaria from "./bulgaria.svg"
+import Canada from "./canada.svg"
+import China from "./china.svg"
+import CzechRepublic from "./czech-republic.svg"
+import Denmark from "./denmark.svg"
+import Egypt from "./egypt.svg"
+import EU from "./european-union.svg"
+import HongKong from "./hong-kong.svg"
+import Hungary from "./hungary.svg"
+import Iceland from "./iceland.svg"
+import India from "./india.svg"
+import Iran from "./iran.svg"
+import Israel from "./israel.svg"
+import Japan from "./japan.svg"
+import Kazakhstan from "./kazakhstan.svg"
+import Kuwait from "./kuwait.svg"
+import Kyrgyzstan from "./kyrgyzstan.svg"
+import Moldova from "./moldova.svg"
+import NewZealand from "./new-zealand.svg"
+import Norway from "./norway.svg"
+import Poland from "./poland.svg"
+import Qatar from "./qatar.svg"
+import Romania from "./romania.svg"
+import Russia from "./russia.svg"
+import Serbia from "./serbia.svg"
+import Singapore from "./singapore.svg"
+import SouthAfrica from "./south-africa.svg"
+import SouthKorea from "./south-korea.svg"
+import Sweden from "./sweden.svg"
+import Switzerland from "./switzerland.svg"
+import Tajikistan from "./tajikistan.svg"
+import Turkey from "./turkey.svg"
+import Turkmenistan from "./turkmenistan.svg"
+import Ukraine from "./ukraine.svg"
+import UnitedArabEmirates from "./united-arab-emirates.svg"
+import UnitedKingdom from "./united-kingdom.svg"
+import Uzbekistan from "./uzbekistan.svg"
 
-export { Georgia, Usa }
+export {
+  Georgia,
+  Usa,
+  Armenia,
+  Australia,
+  Azerbaijan,
+  Belarus,
+  Brazil,
+  Bulgaria,
+  Canada,
+  China,
+  CzechRepublic,
+  Denmark,
+  Egypt,
+  EU,
+  HongKong,
+  Hungary,
+  Iceland,
+  India,
+  Iran,
+  Israel,
+  Japan,
+  Kazakhstan,
+  Kuwait,
+  Kyrgyzstan,
+  Moldova,
+  NewZealand,
+  Norway,
+  Poland,
+  Qatar,
+  Romania,
+  Russia,
+  Serbia,
+  Singapore,
+  SouthAfrica,
+  SouthKorea,
+  Sweden,
+  Switzerland,
+  Tajikistan,
+  Turkey,
+  Turkmenistan,
+  Ukraine,
+  UnitedArabEmirates,
+  UnitedKingdom,
+  Uzbekistan,
+}
