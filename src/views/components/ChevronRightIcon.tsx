5b4d65391bb04c6fd3ac7dceef3ac0b5f27b7bf6
Gigi Tophuria <gtophuria@gmail.com>
2021-09-25 00:20:47 +0400

    created chevron icon components

---
+++ src/views/components/ChevronRightIcon.tsx
@@ -0,0 +1,25 @@
+import React, { ReactElement } from "react"
+
+const ChevronRightIcon = ({
+  ...props
+}: React.SVGProps<SVGSVGElement>): ReactElement => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      className="h-6 w-6"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+      {...props}
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth={2}
+        d="M13 5l7 7-7 7M5 5l7 7-7 7"
+      />
+    </svg>
+  )
+}
+
+export default ChevronRightIcon
