a8f3e772ee00b280710a06ee01c2dea6ed59defd
Gigi Tophuria <gtophuria@gmail.com>
2021-09-24 22:53:24 +0400

    added className optional prop to FlagIcon component

--- src/views/components/FlagIcon.tsx
+++ src/views/components/FlagIcon.tsx
@@ -49,100 +49,102 @@ import {
 interface Props {
   readonly code: string
   readonly name: string
+  readonly className?: string
 }
 
-const FlagIcon = ({ code, name }: Props): ReactElement => {
+const FlagIcon = ({ code, name, className }: Props): ReactElement => {
+  const classNames = "h-7 " + className
   switch (code) {
     case "GEL":
-      return <img src={Georgia} className="h-7" alt={name} />
+      return <img className={classNames} src={Georgia} alt={name} />
     case "USD":
-      return <img src={Usa} className="h-7" alt={name} />
+      return <img className={classNames} src={Usa} alt={name} />
     case "AED":
-      return <img src={UnitedArabEmirates} className="h-7" alt={name} />
+      return <img className={classNames} src={UnitedArabEmirates} alt={name} />
     case "AMD":
-      return <img src={Armenia} className="h-7" alt={name} />
+      return <img className={classNames} src={Armenia} alt={name} />
     case "AUD":
-      return <img src={Australia} className="h-7" alt={name} />
+      return <img className={classNames} src={Australia} alt={name} />
     case "AZN":
-      return <img src={Azerbaijan} className="h-7" alt={name} />
+      return <img className={classNames} src={Azerbaijan} alt={name} />
     case "BGN":
-      return <img src={Bulgaria} className="h-7" alt={name} />
+      return <img className={classNames} src={Bulgaria} alt={name} />
     case "BRL":
-      return <img src={Brazil} className="h-7" alt={name} />
+      return <img className={classNames} src={Brazil} alt={name} />
     case "BYN":
-      return <img src={Belarus} className="h-7" alt={name} />
+      return <img className={classNames} src={Belarus} alt={name} />
     case "CAD":
-      return <img src={Canada} className="h-7" alt={name} />
+      return <img className={classNames} src={Canada} alt={name} />
     case "CHF":
-      return <img src={Switzerland} className="h-7" alt={name} />
+      return <img className={classNames} src={Switzerland} alt={name} />
     case "CNY":
-      return <img src={China} className="h-7" alt={name} />
+      return <img className={classNames} src={China} alt={name} />
     case "CZK":
-      return <img src={CzechRepublic} className="h-7" alt={name} />
+      return <img className={classNames} src={CzechRepublic} alt={name} />
     case "DKK":
-      return <img src={Denmark} className="h-7" alt={name} />
+      return <img className={classNames} src={Denmark} alt={name} />
     case "EGP":
-      return <img src={Egypt} className="h-7" alt={name} />
+      return <img className={classNames} src={Egypt} alt={name} />
     case "EUR":
-      return <img src={EU} className="h-7" alt={name} />
-    case "GPB":
-      return <img src={UnitedKingdom} className="h-7" alt={name} />
+      return <img className={classNames} src={EU} alt={name} />
+    case "GBP":
+      return <img className={classNames} src={UnitedKingdom} alt={name} />
     case "HKD":
-      return <img src={HongKong} className="h-7" alt={name} />
+      return <img className={classNames} src={HongKong} alt={name} />
     case "HUF":
-      return <img src={Hungary} className="h-7" alt={name} />
+      return <img className={classNames} src={Hungary} alt={name} />
     case "ILS":
-      return <img src={Israel} className="h-7" alt={name} />
+      return <img className={classNames} src={Israel} alt={name} />
     case "INR":
-      return <img src={India} className="h-7" alt={name} />
+      return <img className={classNames} src={India} alt={name} />
     case "IRR":
-      return <img src={Iran} className="h-7" alt={name} />
+      return <img className={classNames} src={Iran} alt={name} />
     case "ISK":
-      return <img src={Iceland} className="h-7" alt={name} />
+      return <img className={classNames} src={Iceland} alt={name} />
     case "JPY":
-      return <img src={Japan} className="h-7" alt={name} />
+      return <img className={classNames} src={Japan} alt={name} />
     case "KGS":
-      return <img src={Kyrgyzstan} className="h-7" alt={name} />
+      return <img className={classNames} src={Kyrgyzstan} alt={name} />
     case "KRW":
-      return <img src={SouthKorea} className="h-7" alt={name} />
+      return <img className={classNames} src={SouthKorea} alt={name} />
     case "KWD":
-      return <img src={Kuwait} className="h-7" alt={name} />
+      return <img className={classNames} src={Kuwait} alt={name} />
     case "KZT":
-      return <img src={Kazakhstan} className="h-7" alt={name} />
+      return <img className={classNames} src={Kazakhstan} alt={name} />
     case "MDL":
-      return <img src={Moldova} className="h-7" alt={name} />
+      return <img className={classNames} src={Moldova} alt={name} />
     case "NOK":
-      return <img src={Norway} className="h-7" alt={name} />
+      return <img className={classNames} src={Norway} alt={name} />
     case "NZD":
-      return <img src={NewZealand} className="h-7" alt={name} />
+      return <img className={classNames} src={NewZealand} alt={name} />
     case "PLN":
-      return <img src={Poland} className="h-7" alt={name} />
+      return <img className={classNames} src={Poland} alt={name} />
     case "QAR":
-      return <img src={Qatar} className="h-7" alt={name} />
+      return <img className={classNames} src={Qatar} alt={name} />
     case "RON":
-      return <img src={Romania} className="h-7" alt={name} />
+      return <img className={classNames} src={Romania} alt={name} />
     case "RSD":
-      return <img src={Serbia} className="h-7" alt={name} />
+      return <img className={classNames} src={Serbia} alt={name} />
     case "RUB":
-      return <img src={Russia} className="h-7" alt={name} />
+      return <img className={classNames} src={Russia} alt={name} />
     case "SGD":
-      return <img src={Singapore} className="h-7" alt={name} />
+      return <img className={classNames} src={Singapore} alt={name} />
     case "TJS":
-      return <img src={Tajikistan} className="h-7" alt={name} />
+      return <img className={classNames} src={Tajikistan} alt={name} />
     case "TMT":
-      return <img src={Turkmenistan} className="h-7" alt={name} />
+      return <img className={classNames} src={Turkmenistan} alt={name} />
     case "TRY":
-      return <img src={Turkey} className="h-7" alt={name} />
+      return <img className={classNames} src={Turkey} alt={name} />
     case "UAH":
-      return <img src={Ukraine} className="h-7" alt={name} />
+      return <img className={classNames} src={Ukraine} alt={name} />
     case "UZS":
-      return <img src={Uzbekistan} className="h-7" alt={name} />
+      return <img className={classNames} src={Uzbekistan} alt={name} />
     case "ZAR":
-      return <img src={SouthAfrica} className="h-7" alt={name} />
+      return <img className={classNames} src={SouthAfrica} alt={name} />
     case "SEK":
-      return <img src={Sweden} className="h-7" alt={name} />
+      return <img className={classNames} src={Sweden} alt={name} />
     default:
-      return <img src={Georgia} className="h-7" alt="no flag" />
+      return <img className={classNames} src={Georgia} alt="no flag" />
   }
 }
 
